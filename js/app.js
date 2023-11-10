54537a521257b80c75c6468700af2ae1d57ab837
Abdullahi Suiudeen <Abdullahi Suiudeen>
2023-11-10 03:53:58 +0100

    sidebar adjusment

--- js/app.js
+++ js/app.js
@@ -1,25 +1,28 @@
-const adayi = document.getElementById('menu-btn')
-const aside = document.getElementById('aside')
+const adayi = document.getElementById("menu-btn");
+const aside = document.getElementById("aside");
 
-document.getElementById('menu-btn').addEventListener('click', navToggle)
-    function navToggle (){
-        adayi.classList.toggle('open')
-        aside.classList.toggle('translate-x-[100%]')
-        aside.classList.toggle('show-sidebar')
-    }
+document.getElementById("menu-btn").addEventListener("click", adami);
 
-//Scroll interation 
+function adami() {
+  adayi.classList.toggle("open");
+  aside.classList.toggle("translate-y-[0]");
+  aside.classList.toggle("show-sidebar");
+}
+window.addEventListener("DOMContentLoaded", function () {
+  aside.classList.remove("show-sidebar");
+});
+//Scroll interation
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting){
-            entry.target.classList.add('show');
-        }else{
-            entry.target.classList.remove('show')
-        }
-    })
-})
-const hiddenElement = document.querySelectorAll('.work')
-const jagoElement = document.querySelectorAll('.ogah')
-hiddenElement.forEach((el) => observer.observe(el))
-jagoElement.forEach((el) => observer.observe(el))
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const hiddenElement = document.querySelectorAll(".work");
+const jagoElement = document.querySelectorAll(".ogah");
+hiddenElement.forEach((el) => observer.observe(el));
+jagoElement.forEach((el) => observer.observe(el));
